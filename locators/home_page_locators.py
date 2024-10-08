a88c1898bae857146fae5bfa0ed9e3421e5c70cd
markj <mark.johnson@homemail.com>
2024-10-08 21:27:04 +0100

    master

--- locators/home_page_locators.py
+++ locators/home_page_locators.py
@@ -5,5 +5,5 @@ class HomePageLocators:
     def __init__(self, page: Page):
         self.page = page
 
-        self.home_page_title = "Euronics Site | Buy Home and Kitchen Appliances Online | Euronics"
+        self.home_page_title = page.locator("title").first.text_content()
         self.cookie_accept_button = page.get_by_role("button", name="Accept All Cookies")
