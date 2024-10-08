60cd7f470785c05f3306e20d62fd88613360e5bd
markj <mark.johnson@homemail.com>
2024-10-08 22:30:41 +0100

    master

--- features/steps/home_page_steps.py
+++ features/steps/home_page_steps.py
@@ -9,13 +9,19 @@ def step_impl(context):
     expect(page).to_have_title(home_page_locators(page).home_page_title)
 
 
+@given(u'the cookie accept button is visible')
+def step_impl(context):
+    page: Page = context.page
+    expect(home_page_locators(page).cookie_accept_button).to_be_visible()
+
+
 @when(u'I click the cookie accept button')
 def step_impl(context):
     page: Page = context.page
     home_page_locators(page).cookie_accept_button.click(timeout=global_timeout)
 
 
-@then(u'the cookie accept button should be hidden')
+@then(u'the cookie accept button should no longer be visible')
 def step_impl(context):
     page: Page = context.page
     expect(home_page_locators(page).cookie_accept_button).to_be_hidden()
@@ -39,7 +45,7 @@ def step_impl(context):
     home_page_locators(page).postcode_search_button.click(timeout=global_timeout)
 
 
-@then(u'the postcode input should be hidden')
+@then(u'the postcode input should no longer be visible')
 def step_impl(context):
     page: Page = context.page
     expect(home_page_locators(page).postcode_input).to_be_hidden()
--- features/verify_header_links.feature
+++ features/verify_header_links.feature
@@ -2,9 +2,10 @@ Feature: Verify Header Links
 
   Scenario: Accept cookies and enter postcode on the home page
     Given I am on the home page
+    And the cookie accept button is visible
     When I click the cookie accept button
-    Then the cookie accept button should be hidden
+    Then the cookie accept button should no longer be visible
     And the postcode input should be visible
     When I enter a postcode
     And I click the search button
-    Then the postcode input should be hidden
+    Then the postcode input should no longer be visible
