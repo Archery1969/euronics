d01a1bbc66e777b2ab02a03a8a81aecaf93dea43
markj <mark.johnson@homemail.com>
2024-10-10 12:01:01 +0100

    master

--- pages/home_page.py
+++ pages/home_page.py
@@ -12,14 +12,14 @@ class HomePage:
     def page_title(self):
         return self.locators.page_title
 
-    def cookie_button_locator(self, action='visible'):
-        return perform_action(self.locators.cookie_accept_button, action)
+    def cookie_button_locator(self, action='visible', fill_value=None):
+        return perform_action(self.locators.cookie_accept_button, action, fill_value)
 
     def postcode_input_locator(self, action='visible', fill_value=None):
         return perform_action(self.locators.postcode_input, action, fill_value)
 
-    def postcode_search_locator(self, action='visible'):
-        return perform_action(self.locators.postcode_search_button, action)
+    def postcode_search_locator(self, action='visible', fill_value=None):
+        return perform_action(self.locators.postcode_search_button, action, fill_value)
 
-    def contact_us_locator(self, action='visible'):
-        return perform_action(self.locators.contact_us_link, action)
+    def contact_us_locator(self, action='visible', fill_value=None):
+        return perform_action(self.locators.contact_us_link, action, fill_value)
--- utils/actions.py
+++ utils/actions.py
@@ -1,15 +1,11 @@
 from playwright.sync_api import Locator
 
 
-def perform_action(locator: Locator = None, action: str = '', fill_value=None):
+def perform_action(locator: Locator = None, action=None, fill_value=None):
     action = action.lower()
-    if action == 'visible' and locator:
-        return locator
-    elif action == 'hidden' and locator:
-        return locator
-    elif action == 'click' and locator:
+    if action == 'click' and locator:
         locator.click()
     elif action == 'fill' and locator and fill_value:
         locator.fill(fill_value)
     else:
-        raise ValueError(f"Action '{action}' is not supported for this element.")
+        return locator
