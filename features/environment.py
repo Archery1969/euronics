1be7be9d18e046f41bd6da0abddcb4f1cf303b51
markj <mark.johnson@homemail.com>
2024-10-11 12:19:43 +0100

    master

--- features/environment.py
+++ features/environment.py
@@ -5,13 +5,16 @@ from playwright.sync_api import sync_playwright
 
 def before_all(context):
     try:
-        context.driver = sync_playwright().start()
-        context.browser = context.driver.chromium.launch(headless=False, slow_mo=0, channel="chrome", args=["--start-maximized"])
         env = context.config.userdata.get("ENV", "dev").lower()
         config_file = f"config/config_{env}.yml"
         with open(config_file, 'r') as file:
             config_data = yaml.safe_load(file)
         context.config.userdata.update(config_data)
+        headless = context.config.userdata.get("headless", True)
+        if isinstance(headless, str):
+            headless = headless.lower() == "true"
+        context.driver = sync_playwright().start()
+        context.browser = context.driver.chromium.launch(headless=headless, slow_mo=0, channel="chrome", args=["--start-maximized"])
     except Exception as e:
         raise AssertionError(f"Error in before_all setup: {e}")
 
